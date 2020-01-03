c2c116ab5284e55f438200622eb14414c6cfe233
shimazaki takahiro <shimazaki@lightcafe.co.jp>
2020-01-03 18:56:37 +0900

    データ生成処理

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == 'development'
+  (1..50).each do |i|
+    Board.create(name: "User#{i}", title: "Title#{i}", body: "description\nNo.#{i}")
+  end
+end
